79b9ec2bdb8ea4404d1e898ff58a8c8daa4740de
Britodfbr <britodfbr@gmail.com>
2022-09-08 10:57:03 -0300

    chore: Unificação de versão do projeto

--- incolume/py/workshop/trainee/__init__.py
+++ incolume/py/workshop/trainee/__init__.py
@@ -1,1 +1,14 @@
-__version__ = '0.1.0'
+"""Principal module."""
+from pathlib import Path
+from tomli import load
+
+pyproject = Path(__file__).parents[4] / 'pyproject.toml'
+versionfile = Path(__file__).parent / 'version.txt'
+
+with pyproject.open("rb") as f:
+    versionfile.write_text(
+        f"{load(f)['tool']['poetry']['version']}\n"
+    )
+
+
+__version__ = versionfile.read_text().strip()
